bc813261d5fac8c78e797096aa0bd012595a5e75
delguoqing <delguoqing@hotmail.com>
2015-06-08 00:33:36 +0800

    add quick test for mot file

--- src/quick_tests/test1.py
+++ src/quick_tests/test1.py
@@ -29,11 +29,16 @@ def test_vf(path):
 		u1, v1 = numpy.frombuffer(buffer(data[offset + 0x10: offset + 0x14]), dtype=numpy.dtype(">f2"))
 		u2, v2 = numpy.frombuffer(buffer(data[offset + 0x14: offset + 0x18]), dtype=numpy.dtype(">f2"))
 		nx, ny = get(offset + 0x10, "ff")
-		print "test 0x%04x, 0x%04x, 0x%04x, 0x%04x" % (n1, n2, n3, n4)
+		#print "test 0x%04x, 0x%04x, 0x%04x, 0x%04x" % (n1, n2, n3, n4)
+		
+		# wrong
 		#print "test2 %.2f, %.2f, %.2f, %.2f" % (u1, v1, u2, v2)
 		#print "uv1", u1, v1
 		#print "uv2", u2, v2
+		# wrong
 		#print "test3 %f, %f" % (nx, ny)
+		
+		print "0x%08x 0x%08x" % get(offset + 0x10, "II")
 
 def test_vdata_ex(path):
 	f = open(path, "rb")
@@ -52,9 +57,15 @@ def test_vdata_ex(path):
 		u1, v1 = numpy.frombuffer(buffer(data[offset + 0x4: offset + 0x8]), dtype=numpy.dtype(">f2"))
 		print "color:ARGB = 0x%08x, u1 = %.2f, v2 = %.2f" % (c, u1, v1)
 	
+def test_mot1(path):
+	f = open(path, "rb")
+	data = f.read()
+	f.close()
+	
+	get = get_getter(data, ">")
+	
+
 if __name__ == '__main__':
-	#test_vf(r"..\..\..\..\bayonetta\data03_files\pl\pl001a\pl0031.wmb")
-	#test_vf(r"..\..\..\..\bayonetta\data01_files\bm\bm000a\bm000a.wmb")
-	#test_vdata_ex(r"..\..\..\..\bayonetta\data01_files\bm\bm000a\bm000a.wmb")
 	#test_vdata_ex(sys.argv[1])
-	test_vf(sys.argv[1])
+	#test_vf(sys.argv[1])
+	test_mot1(sys.argv[1])

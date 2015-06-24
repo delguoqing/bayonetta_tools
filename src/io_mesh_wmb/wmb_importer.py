79553810630ed792061214b42ae818ce36fd4826
delguoqing <delguoqing@hotmail.com>
2015-06-24 23:48:47 +0800

    organize code as package

---
+++ src/io_mesh_wmb/wmb_importer.py
@@ -0,0 +1,53 @@
+# -*- coding: utf8 -*-
+import os
+import sys
+
+sys.path.append("parser")
+
+def import_wmb(filepath):
+	import parse
+	f = open(filepath, "rb")
+	wmb = parse.parse(f, False)
+	f.close()
+	
+#################################
+# build blend mesh from neox mesh
+#################################
+def build_blend_mesh(geo_data, submesh_data_list):
+	import bpy
+	import bmesh
+	for i, submesh_data in enumerate(submesh_data_list):
+		# bmesh start
+		bm = bmesh.new()
+		# 	vertices
+		for j in range(submesh_data.v_count):
+			v_idx = (submesh_data.v_start + j) * 3
+			co1 = geo_data.v_list[v_idx]
+			co2 = geo_data.v_list[v_idx + 2]
+			co3 = geo_data.v_list[v_idx + 1]
+			vert = bm.verts.new((co1, co2, co3))
+		bm.verts.index_update()
+		# 	faces
+		for j in range(submesh_data.tri_start,
+						submesh_data.tri_start + submesh_data.tri_count):
+			i1, i2, i3 = geo_data.tri_list[j * 3: j * 3 + 3]
+			# global vertex index -> per mesh vertex index
+			i1 -= submesh_data.v_start	
+			i2 -= submesh_data.v_start
+			i3 -= submesh_data.v_start
+			bm.faces.new((bm.verts[i1], bm.verts[i2], bm.verts[i3]))
+		bm.faces.index_update()
+		# convert to mesh
+		mesh = bpy.data.meshes.new(name=submesh_data.name)
+		bm.to_mesh(mesh)
+		# add uv
+		for j in range(submesh_data.uv_chnl_count):
+			mesh.uv_textures.new("tex%d" % j)
+			for loop in mesh.loops:
+				uv_idx = submesh_data.uv_start + (submesh_data.v_count * j) + loop.vertex_index
+				setattr(mesh.uv_layers[j].data[loop.index], "uv", geo_data.uv_list[uv_idx * 2: uv_idx * 2 + 2])
+		# create object
+		obj = bpy.data.objects.new(submesh_data.name, mesh)
+		bpy.context.scene.objects.link(obj)
+		bpy.context.scene.objects.active = obj
+		obj.select = True
--- src/parser/test.py
+++ src/parser/test.py
@@ -4,12 +4,14 @@ import sys
 from util import iter_path, \
 				 beep_error, beep_finish
 
+DATA_ROOT = r"..\..\..\..\bayonetta"
+
 def iter_all_wmb():
-	return glob.glob(r"..\..\..\bayonetta\*\*\*\*.wmb")
+	return glob.glob(os.path.join(DATA_ROOT, "*\*\*\*.wmb"))
 
 def get_iter_one_pack_wmb(i):
 	def iter_one_pack_wmb():
-		return glob.glob(r"..\..\..\bayonetta\data%02d_files\*\*\*.wmb" % i)
+		return glob.glob(os.path.join(DATA_ROOT, r"data%02d_files\*\*\*.wmb" % i))
 	return iter_one_pack_wmb
 
 def make_common_test(wmb_files, dump_obj=False):
@@ -35,9 +37,9 @@ def make_common_test(wmb_files, dump_obj=False):
 # all test cases
 ####################
 test_all_wmb = make_common_test(iter_all_wmb)
-test_bm0020 = make_common_test(r"..\..\..\bayonetta\data01_files\bm\bm0020\bm0020.wmb",
+test_bm0020 = make_common_test(os.path.join(DATA_ROOT, r"data01_files\bm\bm0020\bm0020.wmb"),
 							   dump_obj=False)
-test_em001f = make_common_test(r"..\..\..\bayonetta\data01_files\em\em001f\em001f.wmb",
+test_em001f = make_common_test(os.path.join(DATA_ROOT, r"data01_files\em\em001f\em001f.wmb"),
 							   dump_obj=False)
 test_data01 = make_common_test(get_iter_one_pack_wmb(1))
 test_data02 = make_common_test(get_iter_one_pack_wmb(2))

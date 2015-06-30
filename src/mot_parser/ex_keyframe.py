68e56380cad34c6c8bf02a92c03d656ecef67153
delguoqing <delguoqing@hotmail.com>
2015-07-01 00:47:25 +0800

    figured out keyframe, bone_index, frame_index

--- src/mot_parser/ex_keyframe.py
+++ src/mot_parser/ex_keyframe.py
@@ -2,16 +2,15 @@ class cls_obj(object):
 	
 	def __init__(self):
 		"""BEG FIELDS"""
-		self.unk0 = None
-		self.unk1 = None
+		self.bone_index = None
+		self.frame_index = None
+		self.offset = None
 		self.unk2 = None
 		self.unk3 = None
-		self.unk4 = None
-		self.unk5 = None
 		"""END FIELDS"""
 		pass
 		
 	def assert_null(self):
-		assert self.unk0 == 0x7FFF and self.unk1 == -1 \
+		assert self.bone_index == 0x7FFF and self.frame_index == -1 \
 			and not any((self.unk2, self.unk3, self.unk4, self.unk5)), "should be null entry."
 	
--- src/quick_tests/test2.py
+++ src/quick_tests/test2.py
@@ -17,18 +17,34 @@ def parse(f):
 	assert FOURCC == "mot\x00", "invalid mot file"
 	a, b, header_size, entry_count = get(0x4, "HHII")
 	assert header_size == 0x10, "header_size != 0x10"
-	print "%d, %d" % (a, b)
+	print "%d, %d, %d" % (a, b, get(0x4, "I"))
 	entry_end = (header_size + entry_count * 0xc)
 	print "entry_end = %d" % entry_end
 	
 	base_offset = header_size
 	now_off = entry_end
+	last_bone_index = None
+	last_frame_index = None
 	for i in xrange(entry_count):
-		values = get(base_offset + i * 0xc, "6h")
-		print values
+		values = get(base_offset + i * 0xc, "4h")
+		bone_index = values[0]
+		frame_index = values[1]
+		if bone_index != last_bone_index:
+			print
+		else:
+			assert frame_index > last_frame_index
+		int_impl = get(base_offset + i * 0xc + 0x8, "I")
+		float_impl = get(base_offset + i * 0xc + 0x8, "f")
+		if int_impl >= len(data):
+			v = float_impl
+		else:
+			v = int_impl
+		print values[:4], v
 		#if i != entry_count - 1:
-		#	assert values[-1] == now_off
-		#now_off += 12 + 4 * values[2]
+		#	assert values[-1] == now_off, "expect off=%d, off=%d" % (now_off, values[-1])
+		now_off += 12 + 4 * values[2]
+		last_bone_index = bone_index
+		last_frame_index = frame_index
 		
 def aux_parse_wmb(f):
 	data = f.read()
